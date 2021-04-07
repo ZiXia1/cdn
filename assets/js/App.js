a0f12cd05450b980efaf746246a7ca69349a3e38
籽夏 <66472729+ZiXia1@users.noreply.github.com>
2021-04-07 22:33:35 +0800

    Update App.js

--- assets/js/App.js
+++ assets/js/App.js
@@ -15,10 +15,10 @@ eval(function (p, a, c, k, e, r) {
     ;
     while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
     return p
-}('a.f(\' %c 9 1 %c b://d.e/8/1\',\'2:#3;4:#5;6:7 0\',\'2:#5;4:#3;6:7\');', 16, 16, '|lanstar|color|444|background|eee|padding|5px|dyedd|Theme|console|https||github|com|log'.split('|'), 0, {}));
+}('a.f(\' %c 9 1 %c b://d.e/8/1\',\'2:#3;4:#5;6:7 0\',\'2:#5;4:#3;6:7\');', 16, 16, '|cdnmc|color|444|background|eee|padding|5px|dyedd|Theme|console|https||github|com|log'.split('|'), 0, {}));
 
 
-let lanstar = {
+let cdnmc = {
     init: function () {
         this.addCommentInit()
         this.addSearchEvent()
